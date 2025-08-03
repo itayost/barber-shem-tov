7b5b5a52018c313af344723a63e20934445e993b
Itay Ostraich <itayost1@gmail.com>
2025-08-03 15:11:03 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="relative">
       <Hero
-        title="kkצור את העתיד שלך כאמן ספרות מוביל"
+        title="צור את העתיד שלך כאמן ספרות מוביל"
         subtitle="The Fader Academy"
         backgroundImage={[
           '/images/hero/homeHero1.jpg',
